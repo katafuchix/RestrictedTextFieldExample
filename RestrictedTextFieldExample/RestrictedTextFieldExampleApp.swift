9fb30866aa43c7e1fdb0cc2aa13f81b02a555069
katafuchix <katafuchix@gmail.com>
2025-06-19 22:53:44 +0900

    Initial Commit

---
+++ RestrictedTextFieldExample/RestrictedTextFieldExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestrictedTextFieldExampleApp.swift
+//  RestrictedTextFieldExample
+//
+//  Created by cano on 2025/06/19.
+//
+
+import SwiftUI
+
+@main
+struct RestrictedTextFieldExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
